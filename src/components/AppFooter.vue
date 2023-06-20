d9ea1b4e242da13ed4fa00c1e2aa3cd8c15610af
Quintusdev <quintusdev92@gmail.com>
2023-06-20 13:37:21 +0200

    first commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <div>
+        <h2>footer</h2>
+    </div>
+</template>
+
+<style lang="scss">
+    
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <div>
+        <h2>header</h2>
+    </div>
+</template>
+
+<style lang="scss">
+    
+</style>
